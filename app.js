186282f5a8b75c1f0537a14a79426fba79d076cb
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2022-11-11 19:39:53 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || "3000";
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use('/', require('./routes/index'));
+app.use('/login', require('./routes/login'));
+
+app.listen(PORT, console.log('Server is running on port ' + PORT));
